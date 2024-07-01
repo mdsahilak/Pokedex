7e783b3bb65414cc01b691fc0625a61e4d55e2e9
Muhammed Sahil AK <mdsahilak.dev@gmail.com>
2024-07-01 21:44:20 +0100

    Initial Commit

---
+++ Pokedex/PokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexApp.swift
+//  Pokedex
+//
+//  Created by Sahil Ak on 01/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct PokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
