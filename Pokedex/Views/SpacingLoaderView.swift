50147b7920762c8fca902b2c8b7b50e81d7cd1d6
Muhammed Sahil AK <mdsahilak.dev@gmail.com>
2024-07-03 19:11:49 +0100

    add navigation split view support

---
+++ Pokedex/Views/SpacingLoaderView.swift
@@ -0,0 +1,18 @@
+//
+//  SpacingLoaderView.swift
+//  Pokedex
+//
+//  Created by Sahil Ak on 03/07/2024.
+//
+
+import SwiftUI
+
+struct SpacingLoaderView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SpacingLoaderView()
+}
