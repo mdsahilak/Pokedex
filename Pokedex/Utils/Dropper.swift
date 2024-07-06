de30ead833ffa7cf3c140f2dd6e12947e6d420f0
Muhammed Sahil AK <mdsahilak.dev@gmail.com>
2024-07-06 05:19:23 +0100

    Add documentation to dropper

--- Pokedex/Utils/Dropper.swift
+++ Pokedex/Utils/Dropper.swift
@@ -10,17 +10,24 @@ import Drops
 
 /// Helper struct to send Toasts
 struct Dropper {
+    
+    /// Send a UI Toast message to the user
+    /// - Parameters:
+    ///   - type: the type of toast to send, i.e a warning, success or error.
+    ///   - message: the textual message to conveyed to the user.
     static func send(_ type: MessageType, message: MessageItem) {
         Drops.show(
             Drop(title: message.title, subtitle: message.subtitle, icon: type.icon, duration: .seconds(0.9), accessibility: .init(message: "\(message.title), \(message.subtitle ?? "")"))
         )
     }
     
+    /// Represents the message contents
     struct MessageItem {
         var title: String
         var subtitle: String? = nil
     }
     
+    /// Represents the type of message
     enum MessageType {
         case error, warning, success
         
